67999d2a79d27864cb03037f3ff2aed78c40d06d
nitharjank <46797844+nitharjank@users.noreply.github.com>
2020-10-18 03:52:03 -0400

    Promise fulfilled

--- src/data/api.js
+++ src/data/api.js
@@ -11,25 +11,40 @@ function api() {
     Date: Date.now(),
     });
     
-
+    
     const createOrder = (async (expireAt, comments) => {
-        try {
-            const res = await fetch (url, {
-                method: 'POST',
-                headers: headers,
-                body: JSON.stringify({
-                    "expireAt": expireAt,
-                    "comments": comments
+        const promise1 = new Promise((resolve, reject) => {
+            resolve('Success!');
+        });
+          
+        promise1.then((value) => {
+            try {
+                const res = fetch (url, {
+                    method: 'POST',
+                    headers: headers,
+                    body: JSON.stringify({
+                        "expireAt": expireAt,
+                        "comments": comments
+                    })
                 })
-            })
-            const result_as_json = await res.json();
-            return result_as_json
-        } catch(e) {
-            console.log(e);
-        }   
+    
+                const result_as_json = res.json();
+                return result_as_json
+    
+            } catch(e) {
+                console.log(e);
+            }
+            console.log(value);
+        });
+           
     })
+
+    
+    
     let data = createOrder("2020-05-08T14:26:48Z", "Good-Morning");
     console.log(data);
 
 }
+
+
 export default api();
